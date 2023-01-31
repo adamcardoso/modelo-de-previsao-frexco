966527793ee93e01bfb61b77554ccd1eba21f08e
adamcardoso <adamjuniorcardoso@gmail.com>
2023-01-30 21:11:49 -0300

    Finalizando o projeto.

--- main.py
+++ main.py
@@ -11,4 +11,20 @@ except Exception as e:
     print(f'Erro ao carregar dados: {e}')  # tratamento de exceção
     exit(1)  # encerramento do programa
 
+try:
+    plt.plot(data_frame['Vendas'])  # plotagem de gráfico
+    plt.show()  # exibição de gráfico
+except Exception as e:
+    print(f'Erro ao plotar gráfico: {e}')  # tratamento de exceção
+    exit(1)  # encerramento do programa
 
+try:
+    model = ARIMA(data_frame['Vendas'], order=(3, 1, 0))  # criação de modelo ARIMA
+    model_fit = model.fit()  # treinamento de modelo
+    print(model_fit.summary())  # exibição de resumo
+    future_predictions = model_fit.forecast(steps=5)[0]  # previsão de demanda
+    future_predictions = round(future_predictions, 2)  # arredondamento de previsão
+    print("Previsão de demanda para os próximos 5 dias:", future_predictions)  # exibição de previsão
+except Exception as e:
+    print(f'Erro ao treinar ou prever modelo ARIMA: {e}')  # tratamento de exceção
+    exit(1)  # encerramento do programa
